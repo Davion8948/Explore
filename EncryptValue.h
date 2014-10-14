bae8e04b73ae10ff3b1c15a3f82a92873d4d2660
xiaochen <xiaochen200642@gmail.com>
2014-10-14 21:06:17 +0800

    change encryptvalue.h

--- EncryptValue.h
+++ EncryptValue.h
@@ -1,5 +1,5 @@
 #pragma once
-#define DEBUG_OPEN 1;
+#define DEBUG_OPEN 1
 template<class T>
 class EncryptValue
 {
@@ -35,4 +35,126 @@ private:
 #endif
 
 };
+template<class T>
+EncryptValue<T>::EncryptValue(void)
+{
+	setValue(0);
+}
 
+template<class T>
+EncryptValue<T>::~EncryptValue(void)
+{
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator=(EncryptValue<T> other)
+{
+	setValue(other.getValue());
+	return *this;
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator=(T value)
+{
+	setValue(value);
+	return *this;
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator+=(EncryptValue<T> other)
+{
+	setValue(getValue() + other.getValue());
+	return *this;
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator += (T value)
+{
+	setValue(getValue() + value);
+	return *this;
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator-=(EncryptValue<T> other)
+{
+	setValue(getValue() - other.getValue());
+	return *this;
+}
+template<class T>
+EncryptValue<T> EncryptValue<T>::operator-=(T value)
+{
+	setValue(getValue() - value);
+	return *this;
+}
+template<class T>
+bool EncryptValue<T>::operator==(EncryptValue<T> other)
+{
+	return getValue() == other.getValue();
+}
+template<class T>
+bool EncryptValue<T>::operator==(T value)
+{
+	return getValue() == value;
+}
+template<class T>
+bool EncryptValue<T>::operator!=(EncryptValue<T> other)
+{
+	return getValue() != other.getValue();
+}
+template<class T>
+bool EncryptValue<T>::operator!=(T value)
+{
+	return getValue() != value;
+}
+template <class T>
+bool EncryptValue<T>::operator > (EncryptValue<T> other)
+{
+	return getValue() > other.getValue();
+}
+template<class T>
+bool EncryptValue<T>::operator > (T value)
+{
+	return getValue() > value;
+}
+template<class T> 
+bool EncryptValue<T>::operator >= (EncryptValue<T> other)
+{
+	return getValue() >= other.getValue();
+}
+template<class T>
+bool EncryptValue<T>::operator >= (T value)
+{
+	return getValue() >= value;
+}
+template <class T>
+bool EncryptValue<T>::operator < (EncryptValue<T> other)
+{
+	return getValue() < other.getValue();
+}
+template <class T>
+bool EncryptValue<T>::operator < (T value)
+{
+	return getValue() < value;
+}
+template<class T>
+bool EncryptValue<T>::operator<=(EncryptValue<T> other)
+{
+	return getValue() <= other.getValue();
+}
+template <class T>
+bool EncryptValue<T>::operator<=(T value)
+{
+	return getValue() <= value;
+}
+template <class T>
+T EncryptValue<T>::getValue()
+{
+	return mValue * mEncryptValue;
+}
+template <class T>
+void EncryptValue<T>::setValue(T value)
+{
+	mValue = value;
+	mEncryptValue = (T)rand();
+	mValue ^= mEncryptValue;
+
+#if DEBUG_OPEN
+	mRealVal = value;
+#endif
+
+}
